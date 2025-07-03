2ef1127a8d6e054fae50b8c480a39faa2f98ad40
Nesailormun <mun.stas@mail.ru>
2025-07-03 20:06:36 +0300

    Исправлены ошибки в тестах Postman. Изменено содержимое docker-compose.yml для корректной сборки проекта в контейнерах.

--- stats-service/stats-dto/src/main/java/ru/practicum/dto/CreateEndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/dto/CreateEndpointHitDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -17,12 +18,16 @@ public class CreateEndpointHitDto {
     @NotNull(message = "Название сервиса не должно быть null")
     @NotBlank(message = "Название сервиса не должно быть пустым.")
     private String app;
+
     @NotNull(message = "URI не должен быть null")
     @NotBlank(message = "URI не должен быть пустым")
     private String uri;
+
     @NotBlank(message = "IP пользователя не должен быть пустым")
     @NotNull(message = "IP пользователя не должен быть null")
     private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @NotNull(message = "Время создания не должен быть null")
     private LocalDateTime timestamp;
 }
